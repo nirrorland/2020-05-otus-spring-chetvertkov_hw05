97b85ff4578f7a43f59a1f5116974f964950dcc5
Kirill CHetvertkov <irmo.silindo@gmail.com>
2020-12-19 20:19:39 +0300

    - initial commit hw-07;

--- src/main/java/ru/otus/spring/dao/AuthorDaoJpa.java
+++ src/main/java/ru/otus/spring/dao/AuthorDaoJpa.java
@@ -1,40 +1,40 @@
-package ru.otus.spring.dao;
-
-import org.springframework.stereotype.Repository;
-import ru.otus.spring.domain.Author;
-
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import java.util.List;
-
-@Repository
-public class AuthorDaoJpa implements AuthorDao {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    @Override
-    public Author getById(long id) {
-        return em.find(Author.class, id);
-    }
-
-    @Override
-    public List<Author> getAll() {
-        return em.createQuery("select a from Author a", Author.class).getResultList();
-    }
-
-    @Override
-    public Author getByName(String name) {
-        TypedQuery<Author> query = em.createQuery(
-                "SELECT a FROM Author a where lower(a.name) = lower(:name)"
-                , Author.class);
-        query.setParameter("name", name);
-        try {
-            return query.getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
-}
+//package ru.otus.spring.dao;
+//
+//import org.springframework.stereotype.Repository;
+//import ru.otus.spring.domain.Author;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.NoResultException;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.TypedQuery;
+//import java.util.List;
+//
+//@Repository
+//public class AuthorDaoJpa implements AuthorDao {
+//
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    @Override
+//    public Author getById(long id) {
+//        return em.find(Author.class, id);
+//    }
+//
+//    @Override
+//    public List<Author> getAll() {
+//        return em.createQuery("select a from Author a", Author.class).getResultList();
+//    }
+//
+//    @Override
+//    public Author getByName(String name) {
+//        TypedQuery<Author> query = em.createQuery(
+//                "SELECT a FROM Author a where lower(a.name) = lower(:name)"
+//                , Author.class);
+//        query.setParameter("name", name);
+//        try {
+//            return query.getSingleResult();
+//        } catch (NoResultException e) {
+//            return null;
+//        }
+//    }
+//}
